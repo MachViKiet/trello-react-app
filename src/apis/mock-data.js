b94982420e96d1d85e75555a982b374ef20fed23
Mạch Vĩ Kiệt <130826069+MachViKiet@users.noreply.github.com>
2023-10-11 22:16:29 +0700

    update mockdata

--- src/apis/mock-data.js
+++ src/apis/mock-data.js
@@ -20,7 +20,7 @@ export const mockData = {
             columnId: 'column-id-01',
             title: 'Title of card 01',
             description: 'Markdown Syntax (sẽ ở khóa nâng cao nhé)',
-            cover: 'https://trungquandev.com/wp-content/uploads/2022/07/fair-mern-stack-advanced-banner-trungquandev.jpg',
+            cover: 'https://images01.nicepagecdn.com/page/10/56/website-template-preview-1056330.jpg',
             memberIds: ['test-user-id-01'],
             comments: ['test comment 01', 'test comment 02'],
             attachments: ['test attachment 01', 'test attachment 02', 'test attachment 03']
